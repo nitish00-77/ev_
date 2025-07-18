1ac631d631de92346e4ee2b8d0fc005feff83def
nitish00-77 <nitishkumar32077@gmail.com>
2025-07-18 22:45:44 +0530

    Create ev.py

---
+++ ev.py
@@ -0,0 +1,53 @@
+import joblib
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+df = pd.read_csv("Electric_Vehicle_Population_By_County.csv")
+df.head() 
+df.info()
+# Compute Q1 and Q3
+Q1 = df['Percent Electric Vehicles'].quantile(0.25)
+Q3 = df['Percent Electric Vehicles'].quantile(0.75)
+IQR = Q3 - Q1
+
+# Define outlier boundaries
+lower_bound = Q1 - 1.5 * IQR
+upper_bound = Q3 + 1.5 * IQR
+print('lower_bound:', lower_bound)
+print('upper_bound:', upper_bound)
+
+# Identify outliers
+outliers = df[(df['Percent Electric Vehicles'] < lower_bound) | (df['Percent Electric Vehicles'] > upper_bound)]
+print("Number of outliers in 'Percent Electric Vehicles':", outliers.shape[0])
+# Converts the "Date" column to actual datetime objects
+df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
+
+# Removes rows where "Date" conversion failed
+df = df[df['Date'].notnull()]
+
+# Removes rows where the target (EV Total) is missing
+df = df[df['Electric Vehicle (EV) Total'].notnull()]
+
+# Fill missing values
+df['County'] = df['County'].fillna('Unknown')
+df['State'] = df['State'].fillna('Unknown')
+
+# Confirm remaining nulls
+print("Missing after fill:")
+print(df[['County', 'State']].isnull().sum())
+
+df.head()
+# Cap the outliers - it keeps all the data while reducing the skew from extreme values.
+
+df['Percent Electric Vehicles'] = np.where(df['Percent Electric Vehicles'] > upper_bound, upper_bound,
+                                 np.where(df['Percent Electric Vehicles'] < lower_bound, lower_bound, df['Percent Electric Vehicles']))
+
+# Identify outliers
+outliers = df[(df['Percent Electric Vehicles'] < lower_bound) | (df['Percent Electric Vehicles'] > upper_bound)]
+print("Number of outliers in 'Percent Electric Vehicles':", outliers.shape[0])
